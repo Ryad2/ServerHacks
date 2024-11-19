6fd6bb4619d2243d70f4df1d17594265a5624fa7
aouak <ryad.aouak@gmail.com>
2024-11-19 20:43:47 +0100

    week5 easy done

--- pwn02_01/week05/hard/client.py
+++ pwn02_01/week05/hard/client.py
@@ -7,6 +7,7 @@ from Crypto.Cipher import AES
 HOST = 'netsec.net.in.tum.de'  # TODO
 PORT = 20205  # TODO
 IV = b'\x00' * 16
+hof0 = 0x6594b1c75899dd37a3f18dd1f8ea291e
 
 def pkcs7(message: bytes, block_size: int = 16) -> bytes:
     gap_size = block_size - (len(message) % block_size)
@@ -26,23 +27,27 @@ def get_flag():
     s.connect((HOST, PORT))
     sf = s.makefile('rw')  # we use a file abstraction for the sockets
 
-    #msg = b'type=funfact&number=1'
-    msg = b'type=secret&number=1'
+    #msg = b'type=secrets&num'
+    msg = b'ber=1337'
+    #msg = b'0123456789=1&1=1'
+    #msg = b'type=secret&number=1'
     msg_enc = base64.b64encode(msg).decode()
-    iv_enc = base64.b64encode(IV).decode()
-    #mac = calc_cbc_mac(msg, IV, b'1337133713371337')
-    mac = bytes(0x24c46ef4) + bytes(0x11da3deb) + bytes(0x80d2aea6) + bytes(0xc2dfce3b)
+    #iv_enc = base64.b64encode(msg).decode()
+    iv_enc = "JMRu9BHaPeuA0q6mwt/OOw=="
 
+
+
+    values = 0x24c46ef411da3deb80d2aea6c2dfce3b
+    mac = values.to_bytes((values.bit_length() + 7) // 8, byteorder='big')
     mac_enc = base64.b64encode(mac).decode()
-    request = f'{iv_enc};{iv_enc};{mac_enc}\n'
+
+    request = f'{msg_enc};{iv_enc};{mac_enc}\n'
     print(request)
     sf.write(f'{request}\n')
     sf.flush()
     print(sf.readline().rstrip('\n'))
-
     sf.close()
     s.close()
-
 
 if __name__ == '__main__':
     get_flag()
