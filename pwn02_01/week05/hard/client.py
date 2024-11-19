9852f11e0726edc989bb6edebd343411cded89d3
9724763 <9724763@gmail.com>
2024-11-19 23:59:03 +0100

    try two step request

--- pwn02_01/week05/hard/client.py
+++ pwn02_01/week05/hard/client.py
@@ -3,11 +3,10 @@ import base64
 
 from Crypto.Cipher import AES
 
-# Fill in the right target here
-HOST = 'netsec.net.in.tum.de'  # TODO
-PORT = 20205  # TODO
-IV = b'\x00' * 16
-hof0 = 0x6594b1c75899dd37a3f18dd1f8ea291e
+HOST = 'netsec.net.in.tum.de'
+PORT = 20205
+
+MAC_FAIL_PREFIX = 'MAC verification failed: expected '
 
 def pkcs7(message: bytes, block_size: int = 16) -> bytes:
     gap_size = block_size - (len(message) % block_size)
@@ -18,8 +17,35 @@ def calc_cbc_mac(message: bytes, iv: bytes, key: bytes) -> bytes:
     cipher = AES.new(key, AES.MODE_CBC, iv)
     message = pkcs7(message)
     last_block = cipher.encrypt(message)[-16:]
-    return last_block
+    return last_block    
 
+
+def xor(self, other):
+    return bytes(a ^ b for a, b in zip(self, other))
+
+def pad(self, n):
+    return self + (b'0' * (n - len(self)))
+
+def H(sf, m, iv, mac=None):
+    msg_enc = base64.b64encode(m).decode()
+    iv_enc = base64.b64encode(iv).decode()
+    if mac:
+        mac_enc = base64.b64encode(mac).decode()
+    else:
+        # use any mac, assumed to be incorrect
+        mac_enc = base64.b64encode(b'0' * 16).decode()
+    
+    request = f'{msg_enc};{iv_enc};{mac_enc}\n'
+    print(request.rstrip('\n'))
+    sf.write(request)
+    sf.flush()
+    message = sf.readline().rstrip('\n')
+    print(message)
+    if mac == None:
+        mac = message.lstrip(MAC_FAIL_PREFIX)[:32]
+        mac = bytes.fromhex(mac)
+        return mac
+    return message
 
 def get_flag():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -27,25 +53,33 @@ def get_flag():
     s.connect((HOST, PORT))
     sf = s.makefile('rw')  # we use a file abstraction for the sockets
 
-    #msg = b'type=secrets&num'
-    msg = b'ber=1337'
-    #msg = b'0123456789=1&1=1'
-    #msg = b'type=secret&number=1'
-    msg_enc = base64.b64encode(msg).decode()
-    #iv_enc = base64.b64encode(msg).decode()
-    iv_enc = "JMRu9BHaPeuA0q6mwt/OOw=="
+    # final request message:
+    #      0123456789abcdef
+    m1 = b'type=secrets&num'
+    m2 = b'ber=1337'
+    m = m1 + m2
 
+    # xor(iv, m1) = xor(a, b) = x
+    # b must be of length 16, be split by '&' into 2 (or more) parts containing (at least) one '='
+    #     0123456789abcdef
+    b = b'0000=000&000=000'
+    a = xor(m1, b)
+    x = xor(a, b)
+    iv = xor(x, m1)
 
+    h = {}
+    print('request 1')
+    h[x] = H(sf, iv=a, m=b)
 
-    values = 0x24c46ef411da3deb80d2aea6c2dfce3b
-    mac = values.to_bytes((values.bit_length() + 7) // 8, byteorder='big')
-    mac_enc = base64.b64encode(mac).decode()
+    # xor(h[x], m2) = xor(c, b)
+    c = xor(h[x], pad(m2, 16))
 
-    request = f'{msg_enc};{iv_enc};{mac_enc}\n'
-    print(request)
-    sf.write(f'{request}\n')
-    sf.flush()
-    print(sf.readline().rstrip('\n'))
+    print('request 2')
+    z = H(sf, iv=c, m=b)
+
+    print('final request')
+    print(H(sf, m=m, iv=iv, mac=z))
+
     sf.close()
     s.close()
 
