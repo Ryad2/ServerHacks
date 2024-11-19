44a5326110ddc2cd6009c32df9cc448a48f53d34
aouak <ryad.aouak@gmail.com>
2024-11-19 20:10:47 +0100

    week5 easy done

--- pwn02_01/week05/hard/client.py
+++ pwn02_01/week05/hard/client.py
@@ -1,8 +1,23 @@
 import socket
+import base64
+
+from Crypto.Cipher import AES
 
 # Fill in the right target here
 HOST = 'netsec.net.in.tum.de'  # TODO
 PORT = 20205  # TODO
+IV = b'\x00' * 16
+
+def pkcs7(message: bytes, block_size: int = 16) -> bytes:
+    gap_size = block_size - (len(message) % block_size)
+    return message + bytes([gap_size] * gap_size)
+
+
+def calc_cbc_mac(message: bytes, iv: bytes, key: bytes) -> bytes:
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    message = pkcs7(message)
+    last_block = cipher.encrypt(message)[-16:]
+    return last_block
 
 
 def get_flag():
@@ -11,6 +26,19 @@ def get_flag():
     s.connect((HOST, PORT))
     sf = s.makefile('rw')  # we use a file abstraction for the sockets
 
+    #msg = b'type=funfact&number=1'
+    msg = b'type=secret&number=1'
+    msg_enc = base64.b64encode(msg).decode()
+    iv_enc = base64.b64encode(IV).decode()
+    #mac = calc_cbc_mac(msg, IV, b'1337133713371337')
+    mac = bytes(0x24c46ef4) + bytes(0x11da3deb) + bytes(0x80d2aea6) + bytes(0xc2dfce3b)
+
+    mac_enc = base64.b64encode(mac).decode()
+    request = f'{iv_enc};{iv_enc};{mac_enc}\n'
+    print(request)
+    sf.write(f'{request}\n')
+    sf.flush()
+    print(sf.readline().rstrip('\n'))
 
     sf.close()
     s.close()
