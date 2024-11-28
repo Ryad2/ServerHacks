b42d1a85a40fa0303d896466ca6ce3029dec71fc
9724763 <9724763@gmail.com>
2024-11-28 18:02:32 +0100

    use bitarray

--- pwn02_01/week05/easy/client.py
+++ pwn02_01/week05/easy/client.py
@@ -3,9 +3,10 @@ import hashlib
 import socket
 import base64
 import itertools
+import bitarray
 from hashlib import sha256
 
-import bitarray
+#import bitarray
 from Crypto.Cipher import AES
 #from Crypto.Hash import CMAC
 #from cryptography.hazmat.primitives.ciphers import algorithms
@@ -14,10 +15,14 @@ from Crypto.Cipher import AES
 # Fill in the right target here
 HOST = 'netsec.net.in.tum.de'  # TODO
 PORT = 20105  # TODO
-KEY = b"1337_1337_1337_1337"
+KEY = b"1337133713371337"
 IV = b'\x00' * 16
 
+def xor(self, other):
+    return bytes(a ^ b for a, b in zip(self, other))
 
+def byte_and(self, other):
+    return bytes(a & b for a, b in zip(self, other))
 
 def pkcs7(message: bytes, block_size: int = 16) -> bytes:
     gap_size = block_size - (len(message) % block_size)
@@ -27,10 +32,26 @@ def pkcs7(message: bytes, block_size: int = 16) -> bytes:
 def calc_cbc_mac(message: bytes, iv: bytes, key: bytes) -> bytes:
     cipher = AES.new(key, AES.MODE_CBC, iv)
     message = pkcs7(message)
-    for m in itertools.batched(message, 16):
-        iv = cipher.encrypt(m ^ iv)[-16:]
+    # ideal but supported only since python 3.12
+    #for m in itertools.batched(message, 16):
+    for m in batched_it(message, 16):
+        iv = cipher.encrypt(xor(m, iv))[-16:]
     return iv
 
+# from https://stackoverflow.com/a/8998040
+def batched_it(iterable, n):
+    "Batch data into iterators of length n. The last batch may be shorter."
+    # batched('ABCDEFG', 3) --> ABC DEF G
+    if n < 1:
+        raise ValueError('n must be at least one')
+    it = iter(iterable)
+    while True:
+        chunk_it = itertools.islice(it, n)
+        try:
+            first_el = next(chunk_it)
+        except StopIteration:
+            return
+        yield itertools.chain((first_el,), chunk_it)
 
 #def calc_cbc_mac_reference(message: bytes, iv: bytes, key: bytes) -> bytes:
 #    cipher = AES.new(key, AES.MODE_CBC, iv)
@@ -42,9 +63,8 @@ def calc_cbc_mac(message: bytes, iv: bytes, key: bytes) -> bytes:
 #    return hmac.new(key, message, digestmod='sha256').digest()
 
 def calc_hmac(message: bytes, key: bytes) -> bytes:
-    localKey = bytes(key<<128)
-    ipad = bytes((x ^ 0x36) for x in localKey)
-    opad = bytes((x ^ 0x5C) for x in localKey)
+    ipad = xor(key, b'\3\6\3\6\3\6\3\6\3\6\3\6\3\6\3\6')
+    opad = xor(key, b'\5\C\5\C\5\C\5\C\5\C\5\C\5\C\5\C')
     innerHash = sha256(ipad + message).digest()
     outerHash = sha256(opad + innerHash).digest()
     return outerHash
@@ -54,12 +74,12 @@ def calc_hmac(message: bytes, key: bytes) -> bytes:
 
 # inspiration from wikipedia
 def calc_cmac(message: bytes, key: bytes) -> bytes:
-    cipher = AES.new(key, AES.MODE_CBC, 0)
+    cipher = AES.new(key, AES.MODE_CBC, IV)
     #message = pkcs7(message)
-    k0 = int(cipher.encrypt(key)[-32])
+    k0 = int(cipher.encrypt(key)[-16])
 
     def msb(n):
-        return bitarray.bitarray(n)[0]
+        return n & 0x8000000000000000 #bitarray.bitarray(n)[0]
 
     if msb(k0) == 0:
         k1 = k0 << 1
@@ -71,14 +91,24 @@ def calc_cmac(message: bytes, key: bytes) -> bytes:
     else:
         k2 = (k1 << 1) ^ 0x425
 
-    c = 0
-    for m in itertools.batched(message, 16):
-        if len(m) == 16:
+    c = IV
+    for m in batched_it(message, 16):
+        if len(list(m)) == 16:
             #mq = k1 ^ m
-            c = cipher.encrypt(c ^ m)[-16:]
+            c = cipher.encrypt(xor(c, m))[-16:]
         else:
-            mq = (k2 << (16 - len(m)) + 1 << (15 - len(m))) ^ m
-            c = cipher.encrypt(c ^ mq)[-16:]
+            mq = xor(
+                (
+                    k2.to_bytes(16,'big') 
+                    + b'\0' * (16 - len(list(m)))
+                ) + (
+                    (1).to_bytes(16,'big')
+                    + b'\1'
+                    + b'\0' * (15 - len(list(m)))
+                ),
+                m
+            )
+            c = cipher.encrypt(xor(c, mq))[-16:]
     return c
 
 #def calc_cmac_reference(message: bytes, key: bytes) -> bytes:
@@ -95,7 +125,7 @@ def get_flag():
 
     message1 = sf.readline().rstrip('\n')
     print(message1)
-    message1 = bytes(int(message1, 16))
+    message1 = bytes.fromhex(message1)
     answer = f'{base64.b64encode(calc_hmac(message1, KEY))};{base64.b64encode(calc_cbc_mac(message1, IV, KEY))};{base64.b64encode(calc_cmac(message1, KEY))}'
     print(answer)
     sf.write(f'{answer}\n')
